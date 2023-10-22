d1d6edb705a07e26685a26f9f1b46d0b9dd6934f
MisterPrada <lesha.skorpion@gmail.com>
2023-10-22 21:38:21 +0300

    Change config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import glsl from 'vite-plugin-glsl'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import dotenv from 'dotenv';
-import fs from "fs";
 dotenv.config();
 
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
@@ -10,7 +9,7 @@ export default {
     define: {
         __SOCKET_HOST__: `"${process.env.SOCKET_HOST}"`,
         __SOCKET_PORT__: `"${process.env.SOCKET_PORT}"`,
-        __HANDSHAKE_HOST__: `"${process.env.HANDSHAKE_HOST}"`,
+        __HANDSHAKE_HOST__: `"https://handshake.misterprada.com/"`,
     },
     root: 'src/',
     publicDir: '../static/',
@@ -19,10 +18,6 @@ export default {
     {
         host: true,
         open: !isCodeSandbox, // Open if it's not a CodeSandbox
-        https: {
-            key: fs.readFileSync('C:\\OSPanel\\userdata\\config\\cert_files\\server.key', 'utf8'),
-            cert: fs.readFileSync('C:\\OSPanel\\userdata\\config\\cert_files\\server.crt', 'utf8')
-        },
     },
     build:
     {
@@ -32,6 +27,7 @@ export default {
     },
     plugins:
     [
-        glsl()
+        glsl(),
+        basicSsl()
     ]
 }
